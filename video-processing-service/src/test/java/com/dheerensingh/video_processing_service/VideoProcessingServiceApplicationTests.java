c9417ad0060de885d4dd8a9eafdfa4614123c653
dheerensingh <43738284+dheerensingh@users.noreply.github.com>
2025-07-17 02:32:44 +0530

    feat: Initialise Spring Boot project for video processing service

---
+++ video-processing-service/src/test/java/com/dheerensingh/video_processing_service/VideoProcessingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dheerensingh.video_processing_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoProcessingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
